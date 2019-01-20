f8408c07fb0ae9c3a7f43e308565b47432669331
Javier Infante <jabi@irontec.com>
2019-01-20 13:41:24 +0100

    Updates

---
+++ precache-manifest.a1aa18c0d2db831034b641ff99d00ef8.js
@@ -0,0 +1,119 @@
+self.__precacheManifest = [
+  {
+    "url": "1eb7a893589ddce89d81cdb22a356660.ttf"
+  },
+  {
+    "revision": "27ddf47c7a7de1b45858d6d59a4017f5",
+    "url": "notes.html"
+  },
+  {
+    "revision": "9d9be852aec6202f99a33cf9cc292d9c",
+    "url": "95c7b5a26ab51d96ee39f36c66005d67.svg"
+  },
+  {
+    "revision": "eea7cf20786a3256a145",
+    "url": "hljs-langs/powershell.js"
+  },
+  {
+    "revision": "7c8f3c0ae7e7afc7d34b",
+    "url": "hljs-langs/typescript.js"
+  },
+  {
+    "revision": "2a7137bffd9e62556b9b",
+    "url": "hljs-langs/xml.js"
+  },
+  {
+    "revision": "3a9a48de5b7b6b823a80a1d728271367",
+    "url": "index.html"
+  },
+  {
+    "revision": "d189c25e4a3e13402405853731f6dc52",
+    "url": "media/images/ngrx.svg"
+  },
+  {
+    "revision": "fcdf3bb6919b72a13166d6638131457e",
+    "url": "media/images/ngrx.png"
+  },
+  {
+    "revision": "c5e74152a707a5ba2ef642c9aca2b904",
+    "url": "media/logo_irontec.png"
+  },
+  {
+    "revision": "9813c935dc3bc37a150cd7e70c2fb8b8",
+    "url": "media/images/flux-02.png"
+  },
+  {
+    "revision": "15d8fb46f189e267d17affa8b666f353",
+    "url": "media/images/flux-01.png"
+  },
+  {
+    "revision": "136972c1a2ef333c15f5ea83b8c7a3b6",
+    "url": "media/images/flux.png"
+  },
+  {
+    "revision": "d6eb6dd72226f1ee8210",
+    "url": "hljs-langs/shell.js"
+  },
+  {
+    "revision": "8ec6268e47b6b2ca18a0b054471be6b4",
+    "url": "3rd.party.LICENSES"
+  },
+  {
+    "url": "ec4c9962ba54eb91787aa93d361c10a8.ttf"
+  },
+  {
+    "url": "fc84e998bc29b297ea20321e4c90b6ed.ttf"
+  },
+  {
+    "url": "ee7b96fa85d8fdb8c126409326ac2d2b.ttf"
+  },
+  {
+    "url": "d1efcd4d126837fe0dcf9b6cf3a00d64.ttf"
+  },
+  {
+    "revision": "0e8db9865a25d89cca1c",
+    "url": "app.css"
+  },
+  {
+    "url": "e9906e0e5d89da17edd6c9aaa50116d4.jpg"
+  },
+  {
+    "revision": "0e8db9865a25d89cca1c",
+    "url": "app.js"
+  },
+  {
+    "revision": "6e29ff6d4e03bd5ba8b3d5556174789e",
+    "url": "b97c530867a61ea60506b10a416762c4.svg"
+  },
+  {
+    "revision": "e688707ed86fca73e90fd1e2d3dc4645",
+    "url": "a0296bb86117cab30ef27fd515a87f79.svg"
+  },
+  {
+    "url": "89e2666c24d37055bcb60e9d2d9f7e35.ttf"
+  },
+  {
+    "revision": "8a63aaf795c65b9a001dfc6bf7387293",
+    "url": "66cec3167484d42c1ccc513c6259a4f8.svg"
+  },
+  {
+    "url": "50705c5ed1205b63efdbfee941a6b655.ttf"
+  },
+  {
+    "url": "42bbe4eefcde1297b11dc4b6491e9746.ttf"
+  },
+  {
+    "url": "3e1af3ef546b9e6ecef9f3ba197bf7d2.ttf"
+  },
+  {
+    "revision": "5d039d9f3408836ef24841905f04b69b",
+    "url": "1ba934df20c60bfbda92ae00e4ac1b37.svg"
+  },
+  {
+    "revision": "43b843880f019c3bdf2a047b201961a7",
+    "url": "187a99d3fe4e62c75737c5f4a93e00a6.svg"
+  },
+  {
+    "url": "0fc25386220a58203994ce45fb4ae570.ttf"
+  }
+];
